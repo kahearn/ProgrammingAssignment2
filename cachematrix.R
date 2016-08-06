55e6608074d88fdf0794762466a5855a2ad19de2
kahearn <kahearn.ng@gmail.com>
2016-08-06 16:31:48 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are 2 functions in this R code. The first function creates a special "matrix" object and
+## the second function return the inverse of the matrix.
 
 ## Write a short comment describing this function
+## This function creates a special "matrix" object and cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m <- NULL
+## set the value of the new matrix to the cache and set the inverse matrix in cache to null
+  set <- function(y) 
+  {
+    x <<- y
+    m <<- NULL
+  }
+  
+## get the value of the matrix from the cache
+  get <- function() x
 
+## set the value of the inverse matrix of the cache matrix into cache  
+  setinverse <- function(solve) m <<- solve
+  
+## get the value of the inverse matrix and return the updated list
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated and the "matrix" has not changed, then the cachesolve 
 
-## Write a short comment describing this function
+## return the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) 
+{
+## get the value of the inverse matrix from the cache and return the value of the inverse matrix. 
+## If there is no inverse matrix value in the cache, it will calculate the inverse matrix.
+  m <- x$getinverse()
+  if(!is.null(m))
+    {
+    message("getting cached data")
+    return(m)
+    }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+## Return a matrix that is the inverse of 'x'
+  m
 }
